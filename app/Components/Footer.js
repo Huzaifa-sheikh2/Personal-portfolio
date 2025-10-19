0519ea03039ad198c7c4284492214f5e4ea8bd88
Muhammad Huzaifa <huzaifa818481@gmail.com>
2025-10-19 22:27:08 +0500

    create footer

---
+++ app/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="w-full bg-gray-900 text-white flex items-center justify-center fixed bottom-0 h-16">
+      <h1>This is footer</h1>
+    </div>
+  )
+}
+
+export default Footer
